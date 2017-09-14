15f2070fd12755dd0be0092efd36583335f7529e
Alex2207 <ignatenko2207@gmail.com>
2017-09-14 12:33:48 +0300

    Made changes for linux

--- AmazonSearch/src/com/amazoninvestorclub/StartProgramm.java
+++
@@ -1,9 +0,0 @@
-package com.amazoninvestorclub;
-
-public class StartProgramm {
-
-	public static void main(String[] args) {
-		InputWindow windowWithParams = new InputWindow("Search window");
-	}
-
-}
